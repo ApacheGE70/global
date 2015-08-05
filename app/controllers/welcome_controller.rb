958082c5e595cbbd5d71ba5246b0854f8f26bce8
Apache <jt.lidenbrock@gmail.com>
2015-08-05 23:55:39 +0300

    Fix some troubles and updated pages-proofs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,19 @@
 class WelcomeController < ApplicationController
 	def index
-		@websites_best = Website.order(:likes).reverse_order.limit(5)
-		@websites_last = Website.order(:created_at).reverse_order.limit(5)
+		@websites_best = Website.order(:likes).reverse_order.limit(4)
+		@websites_last = Website.order(:created_at).reverse_order.limit(4)
+		@websites_count = @websites_best.count
 	end
 
 	def all
 		@websites_all = Website.order(:likes)
-		@columns = @websites_all.count / 4
-		@rest = @websites_all.count % 4
+		@rows = @websites_all.count / 3
+		@rest = @websites_all.count % 3
 	end
 	
 	def author
 		@websites_author = Website.order(:user_id)
-		@columns = @websites_author.count / 4
-		@rest = @websites_author.count % 4
+		@columns = @websites_author.count / 3
+		@rest = @websites_author.count % 3
 	end
 end
